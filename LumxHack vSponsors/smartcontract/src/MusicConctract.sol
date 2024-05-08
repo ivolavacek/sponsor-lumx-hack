72e938a6f9142ac6007418bfbdaf4028e819dbf5
Carlos <iamcarllosjr@gmail.com>
2024-05-07 21:40:39 -0300

    edit smartcontract

--- LumxHack vSponsors/smartcontract/src/MusicConctract.sol
+++ LumxHack vSponsors/smartcontract/src/MusicConctract.sol
@@ -3,9 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/utils/Base64.sol";
 
 /*
   Quem deve passar os parametros para as funções addMember e addPlataforms : Empresário ou Org (Nós) ?
@@ -14,11 +12,12 @@ import "@openzeppelin/contracts/utils/Base64.sol";
   o dinheiro das views das musicas.
 */
 
-contract MusicContract is ERC1155, Ownable, ERC1155Pausable {
+contract MusicContract is ERC1155, ERC1155Pausable {
 
     error URIQueryForNonExistentToken();
     event addedMembers(address[]);
 
+    address public owner;
     Plataform[] public plataforms;
     Members[] public members;
     uint256 public totalReceived;
@@ -34,9 +33,16 @@ contract MusicContract is ERC1155, Ownable, ERC1155Pausable {
     }
 
     // Ao fazer o deploy, é passado os endereços dos membros, e suas respectivas porcentagens
-    constructor(address initialOwner) 
+    constructor() 
     ERC1155("https://ipfs.io/ipfs/QmeNfW3qpdsG5EZK5xPVDjFA8HJskSKxT3rnGCYcXhunns/{id}.json")
-    Ownable(initialOwner) {}
+    {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "is not owner");
+        _;
+    }
 
     function pause() public onlyOwner {
         _pause();
@@ -46,12 +52,13 @@ contract MusicContract is ERC1155, Ownable, ERC1155Pausable {
         _unpause();
     }
 
-    function addMembers(address[] memory members_, uint256[] memory percentages_) public onlyOwner{
+    function addMembers(address[] memory members_, uint256[] memory percentages_) public {
         require(members_.length == percentages_.length, "Mismatch between recipients and percentages arrays");
         
         // Inicializa o array de destinatários
         for (uint256 i = 0; i < members_.length; i++) {
             members.push(Members(members_[i], percentages_[i]));
+            _mint(members_[i], 0, 1, "");
         }
 
         // Adiciona a porcentagem de cada endereço
@@ -65,13 +72,13 @@ contract MusicContract is ERC1155, Ownable, ERC1155Pausable {
 
     // Musica tokenizada
     // Adiciona as plataformas que poderão tocar a musica (E tbm receberaão um NFT quem simboliza a musica)
-    function addPlataforms(address[] memory plataforms_) public onlyOwner{
+    function addPlataforms(address[] memory plataforms_) public {
         // Inicializa o array de endereços das plataformas, e minta um NFT da musica, para dar direito de toca-la
         // Só as plataformas que tem o NFT tem direito de tocar a musica
         for (uint256 i = 0; i < plataforms_.length; i++) {
             plataforms.push(Plataform(plataforms_[i]));
             authorizedPlatform[plataforms_[i]] = true;
-            _mint(plataforms_[i], 0, 1, "");
+            _mint(plataforms_[i], 1, 1, "");
         }
     }
 
@@ -130,5 +137,9 @@ contract MusicContract is ERC1155, Ownable, ERC1155Pausable {
         override(ERC1155, ERC1155Pausable)
     {
         super._update(from, to, ids, values);
+    }
+
+    function returnMembers() public view returns (Members[] memory){
+        return members;
     }
 }
